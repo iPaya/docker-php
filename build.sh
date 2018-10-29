05659dd0375231166c76162fd72406a630d85566
Zhang Di <zhangdi_me@163.com>
2018-10-29 17:09:44 +0800

    Update

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ BASE_PATH=$(cd `dirname $0`; pwd)
 
 docker build -t ipaya/php-7.0:cli $BASE_PATH/7.0/stretch/cli/ \
 && docker build -t ipaya/php-7.0:apache $BASE_PATH/7.0/stretch/apache/ \
-&& docker build -t ipaya/php-7.1:cli $BASE_PATH/7.1/stretch/cli/ \
-&& docker build -t ipaya/php-7.1:apache $BASE_PATH/7.1/stretch/apache/
+&& docker build -t ipaya/php-7.1:cli -f $BASE_PATH/7.1/stretch/cli/Dockerfile $BASE_PATH \
+&& docker build -t ipaya/php-7.1:apache -f $BASE_PATH/7.1/stretch/apache/Dockerfile $BASE_PATH
